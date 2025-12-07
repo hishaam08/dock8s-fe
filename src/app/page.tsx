7fba4e63a67b0886441d0a634ff0b0dccd2f2169
hishaam08 <mdhishaam0807@gmail.com>
2025-12-07 15:35:23 +0530

    removed package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { auth } from "@/auth";
 import LenisProvider from "@/components/LenisProvider";
-import LenisScrollTo from "@/components/LenisScrollTo";
 import Image from "next/image";
 import Link from "next/link";
 
